1a0cb3e4846de3c2420dabb8b0652b864cafeeff
julialwang <julialwa@cs.washington.edu>
2020-10-17 13:54:24 -0700

    made bar prettier, bigger scale

--- frontend/src/search/searchbar.js
+++ frontend/src/search/searchbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const SearchBar = ({input:keyword, onChange:setKeyword}) => {
-  const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
+  const BarStyling = {width:"50rem",height:"3rem",background:"#F2F1F9", border:"none", padding:"1rem", borderRadius:"10px", fontSize:20};
+  
   return (
     <input 
      style={BarStyling}
      key="random1"
      value={keyword}
-     placeholder={"search country"}
+     placeholder={"Search a product"}
      onChange={(e) => setKeyword(e.target.value)}
     />
   );
