915097ad87dcf4546ce21c2a7f0ba975dd7dd917
julialwang <julialwa@cs.washington.edu>
2020-10-17 13:39:22 -0700

    current situation; live update, no button

---
+++ frontend/src/search/searchpage.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import SearchBar from './searchbar';
+import CountryList from './list.js';
+
+const SearchPage = (props) => {
+  const [input, setInput] = useState('');
+  const [countryListDefault, setCountryListDefault] = useState();
+  const [countryList, setCountryList] = useState();
+
+  const fetchData = async () => {
+    return await fetch('https://restcountries.eu/rest/v2/all')
+      .then(response => response.json())
+      .then(data => {
+         setCountryList(data) 
+         setCountryListDefault(data)
+       });}
+
+  const updateInput = async (input) => {
+     const filtered = countryListDefault.filter(country => {
+      return country.name.toLowerCase().includes(input.toLowerCase())
+     })
+     setInput(input);
+     setCountryList(filtered);
+  }
+
+  useEffect( () => {fetchData()},[]);
+	
+  return (
+    <>
+      <h1>Country List</h1>
+      <SearchBar 
+       input={input} 
+       onChange={updateInput}
+      />
+      <CountryList countryList={countryList}/>
+    </>
+   );
+}
+
+export default SearchPage
