3cae279aeec0f0fb640b260add14b60afc0600b3
karansingh098 <koolks2001@outlook.com>
2020-10-18 02:25:31 -0700

    edited server.js

--- DubHacks 2020 Chrome Extension/content.js
+++ DubHacks 2020 Chrome Extension/content.js
@@ -3,5 +3,33 @@ chrome.runtime.onMessage.addListener(function (request) {
     //alert(queryString);
     const urlParams = new URLSearchParams(queryString);
     const product = urlParams.get('k');
-    alert(product);
+    //alert(product);
+
+    /*
+    const MongoClient = require('mongodb').MongoClient;
+    const assert = require('assert');
+    */
+
+    // Connection URL
+    
+
+    const LOCAL_URL = "http://localhost:8080";
+    fetch(LOCAL_URL + "?search=" + product)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
+
+    // Use connect method to connect to the Server
+    /*
+    MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    client.close();
+    });
+    */
+
 })
--- backendGit/server.js
+++ backendGit/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 
 require('dotenv').config();
 
@@ -10,12 +10,31 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-const connection = mongoose.connection;
-connection.once('open', () => {
+const url = 'mongodb+srv://karanvsingh:hello123@cluster0.efpso.gcp.mongodb.net/DubHacks2020?retryWrites=true&w=majority';
+
+const uri = process.env.ATLAS_URI || url;
+//mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+
+//const connection = mongoose.connection;
+
+
+
+let mClient;
+
+MongoClient.connect(url, function(err, client) {
+    mClient = client;
     console.log("MongoDB database connection established successfully");
-})
+    })
+
+
+app.get("/", (req, res) => {
+    let query = req.query.search;
+    const db = mClient.db("DubHacks2020")
+    var greenProducts = db.collection('products').find({name: {$regex : /Carpet/i}}).toArray();
+
+    res.send({ products: greenProducts })
+});
 
 app.listen(port, () => {
     console.log('Server is running on port: 8080');
